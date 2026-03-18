97d7879a62a6e5bec218360fb39934d3fa9b9330
Curran <curran.kelleher@gmail.com>
2026-03-18 09:55:30 -0400

    Add base config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Base config needed for GitHub Pages deploy
+  base: '/constructing-visualizations/',
 })
