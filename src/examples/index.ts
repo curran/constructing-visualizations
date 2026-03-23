f29c4bc9a71382cbaff124116bf866a6fd776073
Curran <curran.kelleher@gmail.com>
2026-03-23 14:51:15 -0400

    Iterate split pane example navigation

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,28 +1,32 @@
 import type { ComponentType } from 'react'
-import { PseudoScatterPlot } from './PseudoScatterPlot'
-import { PseudoBarChart } from './PseudoBarChart'
-import { PseudoLineChart } from './PseudoLineChart'
+import { PseudoScatterPlot } from './001-pseudo-scatter-plot/PseudoScatterPlot'
+import { PseudoBarChart } from './002-pseudo-bar-chart/PseudoBarChart'
+import { PseudoLineChart } from './003-pseudo-line-chart/PseudoLineChart'
 
 export interface Example {
+  id: string
   name: string
   component: ComponentType
 }
 
 export const examples: Example[] = [
   {
+    id: '1',
     name: 'Pseudo Scatter Plot',
     component: PseudoScatterPlot,
   },
   {
+    id: '2',
     name: 'Pseudo Bar Chart',
     component: PseudoBarChart,
   },
   {
+    id: '3',
     name: 'Pseudo Line Chart',
     component: PseudoLineChart,
   },
 ]
 
-export const examplesMap = new Map(examples.map(ex => [ex.name, ex]))
+export const examplesMap = new Map(examples.map(ex => [ex.id, ex]))
 
-export const defaultExample = examples[0].name
+export const defaultExample = '1'
